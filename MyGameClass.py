af6684e16bab4b17015838536f13b0598f6c4400
snchild <sierrachild@yahoo.com>
2023-05-24 13:32:33 -0600

    added player sprite standin

--- MyGameClass.py
+++ MyGameClass.py
@@ -3,7 +3,7 @@ import arcade
 #game class
 class myGame(arcade.Window): #myGame's parent class is arcade.Window
     def __init__(self, width, height, title): #in python, self must be passed in class methods
-        print("initializing myGame...")
+        
         #how to call the parent class and initialize it in python
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.COFFEE) #will be the dirt
@@ -18,7 +18,7 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
         self.player_list = arcade.SpriteList()
         #use_spatial_hash makes it faster to find collisions but slower to move. for walls, that's not a problem
         self.walls_list = arcade.SpriteList(use_spatial_hash=True) 
-        self.player_sprite = arcade.Sprite("images\player_first_draft.png", 0.5) #second parameter is the scale
+        self.player_sprite = arcade.Sprite("images\player_sprite.png", 0.5) #second parameter is the scale
         #origin of the screen is the bottom left corner
         self.player_sprite.center_x = 120
         self.player_sprite.center_y = 120
