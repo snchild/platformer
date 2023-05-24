c2ac9aac2c7fd09deaa94905d8ed67be8e81e9fc
snchild <sierrachild@yahoo.com>
2023-05-24 12:54:53 -0600

    begin adding sprites

--- MyGameClass.py
+++ MyGameClass.py
@@ -6,12 +6,36 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
         print("initializing myGame...")
         #how to call the parent class and initialize it in python
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.csscolor.DIM_GRAY)
+        arcade.set_background_color(arcade.csscolor.DIM_GRAY) #will be the dirt
+
+        self.player_list = None
+        self.walls_list = None
     
     def setup(self):
-        print("underging setup...")
+        #setting up the sprites here makes it easier to incorporate a reset button in the game
+        
+        #set up player sprite
+        self.player_list = arcade.SpriteList()
+        #use_spatial_hash makes it faster to find collisions but slower to move. for walls, that's not a problem
+        self.walls_list = arcade.SpriteList(use_spatial_hash=True) 
+        self.player_sprite = arcade.Sprite("images\player_first_draft.png", 0.5) #second parameter is the scale
+        #origin of the screen is the bottom left corner
+        self.player_sprite.center_x = 120
+        self.player_sprite.center_y = 120
+        self.player_list.append(self.player_sprite)
+
+        #set up walls/obstacles
+        wall_path = "images\ground.png"
+        wall = arcade.Sprite(wall_path, 0.5)
+        wall.position = [360,360]
+        self.walls_list.append(wall)
+
+
 
     def on_draw(self):
         #print("drawing myGame...")
         self.clear()
-        #draw the screen
+
+        #draw the objects on the screen
+        self.player_list.draw()
+        self.walls_list.draw()
