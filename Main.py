5df9230efb6361cb2cfa8f852cc6f14b7350dfc2
snchild <sierrachild@yahoo.com>
2023-05-25 12:21:44 -0600

    changes sprite lists to scene

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import MyGameClass
 SCREEN_HEIGHT = 650 #units: pixels
 SCREEN_WIDTH = 1000
 SCREEN_TITLE = "Sierra's Platformer"
+PLAYER_SPEED = 5 #number of pixels it moves each frame
 
 def main ():
     myWindow = MyGameClass.myGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
--- MyGameClass.py
+++ MyGameClass.py
@@ -9,21 +9,24 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
         arcade.set_background_color(arcade.color.COFFEE) #will be the dirt
 
         self.player_list = None
-        self.walls_list = None
+        self.scene = None #scene object (all the walls)
+        #self.walls_list = None
     
     def setup(self, width, height):
         #setting up the sprites here makes it easier to incorporate a reset button in the game
-        
-        #set up player sprite
-        self.player_list = arcade.SpriteList()
+        #we can use scene instead of player lists and wall lists
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls",use_spatial_hash=True)
         #use_spatial_hash makes it faster to find collisions but slower to move. for walls, that's not a problem
-        self.walls_list = arcade.SpriteList(use_spatial_hash=True) 
+        
+
+
         self.player_sprite = arcade.Sprite("images\player_sprite.png", 0.5) #second parameter is the scale
         #origin of the screen is the bottom left corner
         self.player_sprite.center_x = 120
         self.player_sprite.center_y = 120
-        self.player_list.append(self.player_sprite)
-
+        self.scene.add_aprite("Player", self.player_sprite)
+        
         #set up walls/obstacles
         soil_path = "images\soil_1.png"
 
@@ -32,13 +35,19 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
             #make walls, append them
             wallbottom = arcade.Sprite(soil_path, 0.5)
             wallbottom.position = [x,32]
-            self.walls_list.append(wallbottom)
+            self.scene.add_sprite("Wall",wallbottom)
 
             walltop = arcade.Sprite(soil_path, 0.5)
             walltop.position = [x,height - 32]
-            self.walls_list.append(walltop)
+            self.scene.add_sprite("Wall",walltop)
 
-        
+        #place soil in other specific places
+        wall = arcade.Sprite(soil_path, 0.5)
+        wall.position = [360,360]
+        self.scene.add_sprite("Wall",wall)
+
+        #set up game physics
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.scene.get_sprite_list("Walls"))
 
 
 
@@ -47,5 +56,4 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
         self.clear()
 
         #draw the objects on the screen
-        self.player_list.draw()
-        self.walls_list.draw()
+        self.scene.draw()
