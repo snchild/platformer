700682b029dd0efb515828c29c99319fa98082c8
snchild <sierrachild@yahoo.com>
2023-05-25 15:21:00 -0600

    added gravity

--- Main.py
+++ Main.py
@@ -7,9 +7,10 @@ SCREEN_HEIGHT = 650 #units: pixels
 SCREEN_WIDTH = 1000
 SCREEN_TITLE = "Sierra's Platformer"
 PLAYER_SPEED = 5 #number of pixels it moves each frame
+JUMP_SPEED = 20
 
 def main ():
-    myWindow = MyGameClass.myGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    myWindow = MyGameClass.myGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, PLAYER_SPEED, JUMP_SPEED)
     myWindow.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run() #begins gameplay loop
 
--- MyGameClass.py
+++ MyGameClass.py
@@ -2,24 +2,26 @@
 import arcade
 #game class
 class myGame(arcade.Window): #myGame's parent class is arcade.Window
-    def __init__(self, width, height, title): #in python, self must be passed in class methods
+    def __init__(self, width, height, title, speed, jump_speed): #in python, self must be passed in class methods
         
         #how to call the parent class and initialize it in python
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.COFFEE) #will be the dirt
 
-        self.player_list = None
-        self.scene = arcade.Scene() #scene object (all the walls)
+        self.player_speed = speed
+        self.player_jump_speed = jump_speed
+        self.gravity = 1
+
+        self.scene = None #scene object (all the walls)
         self.physics_engine = None #physics engine
-    
+        self.player_sprite = None
+
     def setup(self, width, height):
         #setting up the sprites here makes it easier to incorporate a reset button in the game
         #we can use scene instead of player lists and wall lists
-        self.scene.add_sprite_list("Player")
-        self.scene.add_sprite_list("Walls",use_spatial_hash=True)
+        self.scene = arcade.Scene() 
         #use_spatial_hash makes it faster to find collisions but slower to move. for walls, that's not a problem
         
-
 
         self.player_sprite = arcade.Sprite("images\player_sprite.png", 0.5) #second parameter is the scale
         #origin of the screen is the bottom left corner
@@ -35,39 +37,34 @@ class myGame(arcade.Window): #myGame's parent class is arcade.Window
             #make walls, append them
             wallbottom = arcade.Sprite(soil_path, 0.5)
             wallbottom.position = [x,32]
-            self.scene.add_sprite("Wall",wallbottom)
+            self.scene.add_sprite("Walls",wallbottom)
 
             walltop = arcade.Sprite(soil_path, 0.5)
             walltop.position = [x,height - 32]
-            self.scene.add_sprite("Wall",walltop)
+            self.scene.add_sprite("Walls",walltop)
 
         #place soil in other specific places
         wall = arcade.Sprite(soil_path, 0.5)
         wall.position = [360,360]
-        self.scene.add_sprite("Wall",wall)
+        self.scene.add_sprite("Walls",wall)
 
         #set up game physics
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.scene.get_sprite_list("Walls"))
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, gravity_constant = self.gravity, walls = self.scene["Walls"])
+        #platforms will be walls that can move
 
     def on_key_press(self, key, modifiers):
-        speed = 5
         #have to used multiple if and elif statements because python has to switch case functions built in
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = speed
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -speed
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = self.player_jump_speed
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -speed
+            self.player_sprite.change_x = -self.player_speed
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = speed
+            self.player_sprite.change_x = self.player_speed
 
     def on_key_release(self, key, modifiers):
         #have to used multiple if and elif statements because python has to switch case functions built in
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
